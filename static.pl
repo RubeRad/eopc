29a6720df05585a3466e649cf156f7de61889615
Reuben Settergren <ruberad@gmail.com>
2018-09-03 07:29:02 -0700

    Append more recent uploads to the static archive

--- static.pl
+++ static.pl
@@ -9,15 +9,65 @@ while (<BOOKS>) {
 }
 
 
-$root = "http://escondido-opc.org/sites/default/files/sermons";
-
-while (<>) {
+$root  = "http://escondido-opc.org/sites/default/files/sermons";
+$root2 = "http://escondido-opc.org/wp-content/uploads/sermons";
+open PARSED, "parsed_mp3s.csv"; # all the old files
+while (<PARSED>) {
   next unless /\S/;
   ($file, $name, $book, $pass) = (split /,/);
   $bookOf{$file} = $book;
   $passOf{$file} = $pass;
   $nameOf{$file} = $name;
   $htmlOf{$file} = qq(<p><a href="$root/sermon_$file">$book $pass ($name)</a>\n);
+}
+close PARSED;
+
+while (<>) { # a dump of all the wp-content/uploads/sermons/20NN/NN/*.mp3
+  # isolate the mp3 filename
+  next if     m!ghost!;
+  next unless m!uploads/sermons/(20\d\d/\d\d)/(.*mp3)!;
+  $dir  = $1;
+  $file = $2;
+
+  # determine the book
+  ($f=$file)=~ s!sermon_!!;
+  $f =~ s!(^[123]?\D+)!!;
+  $book = $1;
+  $book =~ s!_$!!;
+  $book =~ s!\-$!!;
+  die "Can't find $book\n" unless exists $n{$book};
+
+  # Is there a name?
+  if ($f =~ m!\d\.mp3!) { # nope
+    $name = '';
+  } else {
+    $f =~ s!(\D+)\.mp3!!;
+    $name = $1;
+    $name =~ s!_!!g;
+  }
+
+  # determine the passage
+  $pass = '';
+  $f =~ s!\.mp3!!;
+  # at this point should be only chapter and verse numbers left
+  @d = split /\D/, $f;
+  if (@d==1) { # Psalm 45
+    $pass = $d[0];
+  } elsif (@d==3) { # Luke 12:34-56
+    $pass = "$d[0]:$d[1]-$d[2]";
+  } elsif (@d==4) { # Luke 12:34-13:56
+    $pass = "$d[0]:$d[1]-$d[2]:$d[3]";
+  }
+  die "Can't determine passage: $f ($file)\n" unless $pass;
+
+  $bookOf{$file} = $book;
+  $passOf{$file} = $pass;
+  $nameOf{$file} = $name;
+  if ($name) {
+    $htmlOf{$file} = qq(<p><a href="$root2/$dir/$file">$book $pass ($name)</a>\n);
+  } else {
+    $htmlOf{$file} = qq(<p><a href="$root2/$dir/$file">$book $pass</a>\n);
+  }
 }
 
 sub bibleOrder {
